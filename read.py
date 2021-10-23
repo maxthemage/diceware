3ee30ee9ca618c8c4419c27aaf44e1f7d93e19d4
glamourousgecko <glamourousgecko@protonmail.com>
2021-10-22 23:41:22 -0400

    Add main python script for menu generation

--- read.py
+++ read.py
@@ -1,29 +1,52 @@
 #!/usr/bin/python3
 
 import csv, os, time, math
+import roll as load
 
 def checkBreach(password):
     path = "assets/password-breach-data/"
     with open(path + (os.listdir(path)[0])) as csv_object:
         password_data = csv.reader(csv_object)
         try:
+            load.check()
             for row in password_data:
-                print(password+" = "+str(row[0]))
-                time.sleep(0.01)
-                if password.lower()==str(row[0]) or password.title()==str(row[0]) or password.upper()==str(row[0]):
-                    #print(True)
-                    print("\nOops! This password or a similar password was found in a data breach.")
-                    time.sleep(2)
+                breached = f"{str(row[0])}" 
+                if password.lower()==breached or password.title()==breached or password.upper()==breached:
+                    print("                                                                            ", end="\r")
+                    print('\033[1m' + f'\t{breached}' + '\033[0m', end = "\r")
+                    print("\n\n\nOops! This password or a similar password was found in a data breach.")
+                    time.sleep(1)
                     input("Press <ENTER> to continue...")
-                    break
-        except:
+                    break          
+                else:
+                    if breached != "luve12":
+                        print("                                                                            ", end="\r")
+                        print(f"\t{breached}", end = "\r")
+                        time.sleep(0.000001)
+                    else:
+                        print('\033[1m' + '\tNot found' + '\033[0m', end = "\r")
+                        print("\n\nPassword not found among most used passwords in data breaches")
+                        time.sleep(1)
+                        input("Press <ENTER> to continue...")
+        except KeyboardInterrupt:
             print("\nExiting...")
+            time.sleep(1)
 
 def entropy(P, password):
     L = len(password)
     entropy = math.log2(math.pow(P,L))
-    print(f"Password entropy is: {entropy:.2f} bits")
+    print(f"\nPassword entropy is: " + '\033[1m' + f"{entropy:.2f} bits" + '\033[0m')
     time.sleep(2)
+
+def dicewareEntropy():
+    P = 7776
+    L = 5
+    entropy = math.log2(math.pow(P,L))
+    print(f"\n\t• entropy: " + '\033[1m' + f"{entropy:.2f} bits" + '\033[0m')
+    
+
+def dicewareCharacterLength(passphrase):
+    print(f"\t• length: " + '\033[1m' + f"{len(passphrase)} characters" + '\033[0m')
 
 
 def get(password):
@@ -40,8 +63,4 @@ def get(password):
     for line in ranges:
         P += len(line)
     entropy(P, password)
-        
-
-#os.system("clear")
-#password = input("Enter a password to check: ").strip()
-#get(password)
+    dicewareCharacterLength(password)
